15fd328acd2bae72c41cae9dae34dfad07cd81c7
Jarwain <jarwainf@gmail.com>
2016-12-06 05:45:53 -0500

    Submit Button's Back!

--- page/index.php
+++ page/index.php
@@ -166,6 +166,11 @@
             </label>
           </div>
         </div>
+        <div class="form-group">
+          <div class="col-sm-offset-2 col-sm-10">
+            <button type="submit" class="btn btn-primary">Continue</button>
+          </div>
+        </div>
       </form>
   </div> <!-- ./container -->
     <div id="footer">
