56b4ee0d04c86e6631487e87b83fc1d534eb7209
Jarwain <jarwainf@gmail.com>
2017-04-24 18:19:27 -0400

    Student class w/validation

---
+++ search/models/Student.php
@@ -0,0 +1,47 @@
+<?php
+require_once("models/Qualifier.php");
+	class Student{
+		var $znumber;
+		var $qualifications;
+
+		function __construct($znumber){
+			$this->znumber = $znumber;
+			$this->qualifications = array();
+		}
+
+		// returns student if qualifications are valid. else returns false
+		static function validStudent($znumber,$qualifications){
+			$student = new Student($znumber);
+			$student->qualifications = Qualifier::getQualifiers();
+			foreach($qualifications as $key->$val){
+				switch($student->qualifications[$key]->type){
+					case 1: 
+						if($val === 'true' || $val === 'false'){
+							$student->qualifications[$key]->value = 
+								$val === 'true' ? true : false;
+						} else { return false; }
+						break;
+					case 2:
+						$param = json_decode($student->qualifications[$key]->value)->param;
+						$num = floatval($val);
+						if($num >= $param[0] && $num <= $param[1])
+							$student->qualifications[$key]->value = $val;
+						else return false;
+						break;
+					case 3:
+						$param = json_decode($student->qualifications[$key]->value)->param;
+						if(in_array($val,$param))
+							$student->qualifications[$key]->value = $val;
+						else 
+							return false;
+						break;
+					case 4:
+						$param = json_decode($student->qualifications[$key]->value)->param;
+						break;
+				}
+			}
+			return $student;
+		}
+	}
+
+?>
