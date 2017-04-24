e87a2b3c29af6f6c3ddec79b9caec884b7fdaa2d
Jarwain <jarwainf@gmail.com>
2017-04-24 18:55:22 -0400

    Print errors on search page

--- search/search.php
+++ search/search.php
@@ -1,3 +1,6 @@
+<?php ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);?>
 <?php
 require_once("models/Scholarship.php");
 require_once("models/Student.php");
