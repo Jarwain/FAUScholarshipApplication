5f35c9b81f5251af9116dffacd6e71b8b804cb53
Jarwain <jarwainf@gmail.com>
2017-04-24 18:25:43 -0400

    Set up student in search

--- search/models/Student.php
+++ search/models/Student.php
@@ -14,30 +14,32 @@ require_once("models/Qualifier.php");
 			$student = new Student($znumber);
 			$student->qualifications = Qualifier::getQualifiers();
 			foreach($qualifications as $key->$val){
-				switch($student->qualifications[$key]->type){
-					case 1: 
-						if($val === 'true' || $val === 'false'){
-							$student->qualifications[$key]->value = 
-								$val === 'true' ? true : false;
-						} else { return false; }
-						break;
-					case 2:
-						$param = json_decode($student->qualifications[$key]->value)->param;
-						$num = floatval($val);
-						if($num >= $param[0] && $num <= $param[1])
-							$student->qualifications[$key]->value = $val;
-						else return false;
-						break;
-					case 3:
-						$param = json_decode($student->qualifications[$key]->value)->param;
-						if(in_array($val,$param))
-							$student->qualifications[$key]->value = $val;
-						else 
-							return false;
-						break;
-					case 4:
-						$param = json_decode($student->qualifications[$key]->value)->param;
-						break;
+				if(array_key_exists($key,$student->qualifications)){
+					switch($student->qualifications[$key]->type){
+						case 1: 
+							if($val === 'true' || $val === 'false'){
+								$student->qualifications[$key]->value = 
+									$val === 'true' ? true : false;
+							} else { return false; }
+							break;
+						case 2:
+							$param = json_decode($student->qualifications[$key]->value)->param;
+							$num = floatval($val);
+							if($num >= $param[0] && $num <= $param[1])
+								$student->qualifications[$key]->value = $val;
+							else return false;
+							break;
+						case 3:
+							$param = json_decode($student->qualifications[$key]->value)->param;
+							if(in_array($val,$param))
+								$student->qualifications[$key]->value = $val;
+							else 
+								return false;
+							break;
+						case 4:
+							$param = json_decode($student->qualifications[$key]->value)->param;
+							break;
+					}
 				}
 			}
 			return $student;
