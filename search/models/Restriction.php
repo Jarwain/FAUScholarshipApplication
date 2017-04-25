53fca72de0ef4fc31ecd69358f2decb2c6dc2b5f
Jarwain <jarwainf@gmail.com>
2017-04-25 01:44:39 -0400

    Fixed parameter usage

--- search/models/Restriction.php
+++ search/models/Restriction.php
@@ -14,18 +14,18 @@ class Restriction{
 	}
 
 	function qualifies($qualification){
+		$param = $this->valid->param;
 		switch($qualification->type){
 			case 1:
 				if(!($qualification->value == true))
 					return false;
 				break;
 			case 2:
-				$param = $this->valid->param;
 				if(!($qualification->value >= $param[0] && $qualification->value <= $param[1]))
 					return false;
 				break;
 			case 3:
-				if(!(in_array($qualification->value,$this->valid)))
+				if(!(in_array($qualification->value,$param)))
 					return false;
 				break;
 			case 4:
