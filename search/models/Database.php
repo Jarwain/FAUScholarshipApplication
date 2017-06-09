c5a07fc47dce4eebdbf2895d67d63868736bec59
Dyllan To <me@dyllan.to>
2017-06-09 14:55:31 -0400

    Working on refactoring to include Database and Scholarships classes

---
+++ search/models/Database.php
@@ -0,0 +1,25 @@
+<?php
+	class Database{
+		var $link;
+
+		function __construct(){
+			$settings = json_decode(file_get_contents(__DIR__ . "/../.config/database"));
+			$this->link = new \PDO( 'mysql:host='.$settings->host.';dbname='.$settings->dbname.';charset=utf8mb4',
+				$settings->user,
+				$settings->pass,
+				array(
+					\PDO::ATTR_ERRMODE => \PDO::ERRMODE_EXCEPTION,
+					\PDO::ATTR_PERSISTENT => false
+				)
+			);
+		}
+
+		function getScholarshipsJoinRestriction(){
+			$dbRestrictions = $this->link->query("SELECT * FROM `scholarship` s 
+				LEFT JOIN `restriction` r ON s.`code` = r.`sch_code` 
+				WHERE s.`code` like 'TST%'")->fetchAll(PDO::FETCH_COLUMN|PDO::FETCH_GROUP,"code");
+		}
+
+
+	}
+?>
