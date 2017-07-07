829f764666201aeff6ea450e47ab503fbc26d826
Dyllan To <me@dyllan.to>
2017-07-07 16:31:00 -0400

    Finished Qualifications kinda

--- Qualification.sql
+++ Qualification.sql
@@ -2,144 +2,173 @@ INSERT INTO `restriction`
 (qualifier_id, sch_code, category, valid)
 VALUES
 
-(3, SFA810, *, {"param":[2,4]})
+(3, 'SFA810', '*', '{"param":[2,4]}'),
 
-(3, SFA999, *, {"param":[3,4]})
-(4, SFA999, *, {"param":["Freshman","Sophomore","Junior","Senior"]})
-(5, SFA999, *, {"param":[12,18]})
+(3, 'SFA999', '*', '{"param":[3,4]}'),
+(4, 'SFA999', '*', '{"param":["Freshman","Sophomore","Junior","Senior"]}'),
+(5, 'SFA999', '*', '{"param":[12,18]}'),
 
-(1, SFA150, *)
-(2, SFA150, *)
+(1, 'SFA150', '*', ''),
+(2, 'SFA150', '*', ''),
 
-(6, SFA590, *, {"param":["Good"]})
+(6, 'SFA590', '*', '{"param":["Good"]}'),
 
-(1, SFA050, *)
-(3, SFA050, *, "param":["3,4"]})
-(4, SFA050, *, "param":["Sophomore","Junior","Senior","Graduate"]})
+(1, 'SFA050', '*', ''),
+(3, 'SFA050', '*', '{"param":["3,4"]}'),
+(4, 'SFA050', '*', '{"param":["Sophomore","Junior","Senior","Graduate"]}'),
 
-(1, SFA370, *)
+(1, 'SFA370', '*', ''),
 
-(1, SFA035, *)
-(2, SFA035, *)
-(6, SFA035, *, {"param":["Good"]})
+(1, 'SFA035', '*', ''),
+(2, 'SFA035', '*', ''),
+(6, 'SFA035', '*', '{"param":["Good"]}'),
 
-(1, COLSCH, *)
-(2, COLSCH, *)
-(3, COLSCH, *, {"param":[2.5,4]})
-(4, COLSCH, *, {"param":["Freshman","Sophomore","Junior","Senior"]})
-(5, COLSCH, *, {"param":[6,18]})
+(1, 'COLSCH', '*', ''),
+(2, 'COLSCH', '*', ''),
+(3, 'COLSCH', '*', '{"param":[2.5,4]}'),
+(4, 'COLSCH', '*', '{"param":["Freshman","Sophomore","Junior","Senior"]}'),
+(5, 'COLSCH', '*', '{"param":[6,18]}'),
 
-(1, SFA060, *)
-(2, SFA060, *)
-(3, SFA060, *, {"param":[2.5,4]})
-(5, SFA060, *, {"param":[6,18]})
+(1, 'SFA060', '*', ''),
+(2, 'SFA060', '*', ''),
+(3, 'SFA060', '*', '{"param":[2.5,4]}'),
+(5, 'SFA060', '*', '{"param":[6,18]}'),
 
-(1, SFA355, *)
-(2, SFA355, *)
-(6, SFA355, *, {"param":["Good"]})
+(1, 'SFA355', '*', ''),
+(2, 'SFA355', '*', ''),
+(6, 'SFA355', '*', '{"param":["Good"]}'),
 
-(1, SFA861, *)
-(2, SFA861, *)
-(6, SFA861, *, {"param":["Good"]})
+(1, 'SFA861', '*', ''),
+(2, 'SFA861', '*', ''),
+(6, 'SFA861', '*', '{"param":["Good"]}'),
 
-(1, SFA025, *)
-(3, SFA025, a, {"param":[3.2,4]})
-(4, SFA025, a, {"param":["Freshman","Sophomore","Junior","Senior"]})
-(3, SFA025, b, {"param":[3.5,4]})
-(4, SFA025, b, {"param":["Graduate"]})
+(1, 'SFA025', '*', ''),
+(3, 'SFA025', 'a', '{"param":[3.2,4]}'),
+(4, 'SFA025', 'a', '{"param":["Freshman","Sophomore","Junior","Senior"]}'),
+(3, 'SFA025', 'b', '{"param":[3.5,4]}'),
+(4, 'SFA025', 'b', '{"param":["Graduate"]}'),
 
-(1, SFA170, *)
-(2, SFA170, *)
-(3, SFA170, *, {"param":[3.0,4]})
-(5, SFA170, *, {"param":[12,18]})
+(1, 'SFA170', '*', ''),
+(2, 'SFA170', '*', ''),
+(3, 'SFA170', '*', '{"param":[3.0,4]}'),
+(5, 'SFA170', '*', '{"param":[12,18]}'),
 
-(1, SFA900, *)
-(2, SFA900, *)
+(1, 'SFA900', '*', ''),
+(2, 'SFA900', '*', ''),
 
-(1, MEMSCH, *)
-(2, MEMSCH, *)
-(3, MEMSCH, *, {"param":[2.5,4]})
+(1, 'MEMSCH', '*', ''),
+(2, 'MEMSCH', '*', ''),
+(3, 'MEMSCH', '*', '{"param":[2.5,4]}'),
 
-(1, SFA950, *)
-(2, SFA950, *)
-(3, SFA950, *, {"param":[3.5,4]})
-(6, SFA950, *, {"param":["Good"]})
+(1, 'SFA950', '*', ''),
+(2, 'SFA950', '*', ''),
+(3, 'SFA950', '*', '{"param":[3.5,4]}'),
+(6, 'SFA950', '*', '{"param":["Good"]}'),
 
-(4, SFA890, *, {"param":["Freshman","Sophomore","Junior","Senior","Graduate"]})
-(5, SFA890, *, {"param":[9,18]})
+(4, 'SFA890', '*', '{"param":["Freshman","Sophomore","Junior","Senior","Graduate"]}'),
+(5, 'SFA890', '*', '{"param":[9,18]}'),
 
-(1, SFA910, *)
-(2, SFA910, *)
-(4, SFA910, *, {"param":["Freshman","Sophomore","Junior","Senior","Graduate"]})
+(1, 'SFA910', '*', ''),
+(2, 'SFA910', '*', ''),
+(4, 'SFA910', '*', '{"param":["Freshman","Sophomore","Junior","Senior","Graduate"]}'),
 
-(3, SFA500, *, {"param":[3.0,4]})
-(5, SFA500, *, {"param":[12,18]})
+(3, 'SFA500', '*', '{"param":[3.0,4]}'),
+(5, 'SFA500', '*', '{"param":[12,18]}'),
 
-(4, BUSSCH, *, {"param":["Graduate"]})
+(4, 'BUSSCH', '*', '{"param":["Graduate"]}'),
 
-(1, SFA440, *)
-(2, SFA440, *)
-(3, SFA440, *, {"param":[2.5,4]})
+(1, 'SFA440', '*', ''),
+(2, 'SFA440', '*', ''),
+(3, 'SFA440', '*', '{"param":[2.5,4]}'),
 
-(1, SFA840, *)
-(2, SFA840, *)
-(3, SFA840, *, {"param":[3.0,4]})
-(4, SFA840, *, {"param":["Freshman","Sophomore","Junior","Senior","Graduate"]})
+(1, 'SFA840', '*', ''),
+(2, 'SFA840', '*', ''),
+(3, 'SFA840', '*', '{"param":[3.0,4]}'),
+(4, 'SFA840', '*', '{"param":["Freshman","Sophomore","Junior","Senior","Graduate"]}'),
 
-(1, SFA020, *)
-(2, SFA020, *)
-(3, SFA020, *, {"param":[2.0,4]})
-(4, SFA020, *, {"param":[6,18]})
+(1, 'SFA020', '*', ''),
+(2, 'SFA020', '*', ''),
+(3, 'SFA020', '*', '{"param":[2.0,4]}'),
+(4, 'SFA020', '*', '{"param":[6,18]}'),
 
-(1, SFA529, *)
-(2, SFA529, *)
+(1, 'SFA529', '*', ''),
+(2, 'SFA529', '*', ''),
 
-(1, SFA930, *)
-(2, SFA930, *)
-(5, SFA930, *, {"param":[12,18]})
+(1, 'SFA930', '*', ''),
+(2, 'SFA930', '*', ''),
+(5, 'SFA930', '*', '{"param":[12,18]}'),
 
-(1, SFA871, *)
-(2, SFA871, *)
-(3, SFA871, *, {"param":[3.25,4]})
-(4, SFA871, *, {"param":["Freshman","Sophomore","Junior","Senior","Graduate"]})
+(1, 'SFA871', '*', ''),
+(2, 'SFA871', '*', ''),
+(3, 'SFA871', '*', '{"param":[3.25,4]}'),
+(4, 'SFA871', '*', '{"param":["Freshman","Sophomore","Junior","Senior","Graduate"]}'),
 
-(1, SFA360, *)
-(2, SFA360, *)
-(4, SFA360, *, {"param":["Junior","Senior"]})
+(1, 'SFA360', '*', ''),
+(2, 'SFA360', '*', ''),
+(4, 'SFA360', '*', '{"param":["Junior","Senior"]}'),
 
-(1, SFA210, *)
-(2, SFA210, *)
-(4, SFA210, *, {"param":["Sophomore","Junior","Senior"]})
+(1, 'SFA210', '*', ''),
+(2, 'SFA210', '*', ''),
+(4, 'SFA210', '*', '{"param":["Sophomore","Junior","Senior"]}'),
 
-(1, SFA220, *)
-(2, SFA220, *)
-(3, SFA220, *, {"param":[3.0,4]})
+(1, 'SFA220', '*', ''),
+(2, 'SFA220', '*', ''),
+(3, 'SFA220', '*', '{"param":[3.0,4]}'),
 
-(1, SKPSCH, *)
-(2, SKPSCH, *)
-(3, SKPSCH, *, {"param":[3.0,4]})
-(4, SKPSCH, *, {"param":["Freshman","Sophomore","Junior","Senior","Graduate"]})
+(1, 'SKPSCH', '*', ''),
+(2, 'SKPSCH', '*', ''),
+(3, 'SKPSCH', '*', '{"param":[3.0,4]}'),
+(4, 'SKPSCH', '*', '{"param":["Freshman","Sophomore","Junior","Senior","Graduate"]}'),
 
-(3, SGASCH, *, {"param":[2.5,4]})
-(4, SGASCH, *, {"param":["Freshman","Sophomore","Junior","Senior","Graduate"]})
-(5, SGASCH, *, {"param":[6,18]})
+(3, 'SGASCH', '*', '{"param":[2.5,4]}'),
+(4, 'SGASCH', '*', '{"param":["Freshman","Sophomore","Junior","Senior","Graduate"]}'),
+(5, 'SGASCH', '*', '{"param":[6,18]}'),
 
-(7, SFA710, *, {"param":["Communication Sciences and Disorders", "Counselor Education", "Curriculum", "Culture and Education Inquiry", "Educational Leadership", "Exceptional Student Education", "Exercise Science and Health Promotion. Teaching and Learning", "Anthropology", "Communication and Multimedia Studies", "English", "History", "Music", "Philosophy", "Political Science", "Sociology", "Theater and Dance", "Visual Arts and Art History", "Women", "Gender and Sexuality Studies", "Languages", "Linguistics and Comparative Literature"]})
+(7, 'SFA710', '*', '{"param":["Communication Sciences and Disorders", "Counselor Education", "Curriculum", "Culture and Education Inquiry", "Educational Leadership", "Exceptional Student Education", "Exercise Science and Health Promotion. Teaching and Learning", "Anthropology", "Communication and Multimedia Studies", "English", "History", "Music", "Philosophy", "Political Science", "Sociology", "Theater and Dance", "Visual Arts and Art History", "Women", "Gender and Sexuality Studies", "Languages", "Linguistics and Comparative Literature"]}'),
 
+(1, 'SFA100', '*', ''),
+(2, 'SFA100', '*', ''),
+(5, 'SFA100', '*', '{"param":[6,18]}'),
+(4, 'SFA100', '*', '{"param":["Freshman","Sophomore","Junior","Senior","Graduate"]}'),
 
-SFA100
-ADV996
-DENSCH
-SFA455
-SFA140
-SFA041
-SFA520
-RGESCH
-MBGSCH
-MLKSCH
-BOASCH
-TEASCH
-SFA055
-SFA085
+(1, 'ADV996', '*', ''),
+(2, 'ADV996', '*', ''),
+(6, 'ADV996', '*', '{"param":["Good"]}'),
+
+(1, 'DENSCH', '*', ''),
+(2, 'DENSCH', '*', ''),
+(5, 'DENSCH', '*', '{"param":[12,18]}'),
+
+(1, 'SFA140', '*', ''),
+(2, 'SFA140', '*', ''),
+(3, 'SFA140', '*', '{"param":[2.5,4]}'),
+(5, 'SFA140', '*', '{"param":[6,18]}'),
+
+(1, 'SFA041', '*', ''),
+(3, 'SFA041', '*', '{"param":[3.0,4]}'),
+(4, 'SFA041', '*', '{"param":["Sophomore","Junior","Senior","Graduate"]}'),
+
+(2, 'SFA520', '*', ''),
+
+(4, 'MBGSCH', '*', '{"param":["Freshman"]}'),
+
+(1, 'MLKSCH', '*', ''),
+(2, 'MLKSCH', '*', ''),
+(4, 'MLKSCH', '*', '{"param":["Freshman"]}'),
+
+(1, 'BOASCH', '*', ''),
+(2, 'BOASCH', '*', ''),
+(3, 'BOASCH', '*', '{"param":[3.3,4]}'),
+(4, 'BOASCH', '*', '{"param":["Freshman"]}'),
+
+(2, 'TEASCH', '*', ''),
+(3, 'TEASCH', '*', '{"param":[3.0,4]}'),
+
+(2, 'SFA055', '*', ''),
+(3, 'SFA055', '*', '{"param":[3.0,4]}'),
+(5, 'SFA055', '*', '{"param":[12,18]}'),
+
+(3, 'SFA085', '*', '{"param":[3,4]}')
 
 SFA785 ...?
+SFA455 ...?
